f7bb723bd63e6ce9b3bc205674f6e2ec7b29a5c0
AdrianGerman <adrian.german1019@gmail.com>
2023-11-11 11:49:29 -0600

    Link

--- src/Menu.js
+++ src/Menu.js
@@ -1,5 +1,21 @@
+import { Link, NavLink } from "react-router-dom";
+
 function Menu() {
-  return <nav>Menu</nav>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export { Menu };
