68cf83165e26a651c2f4cb70f7fa68f817733987
AdrianGerman <adrian.german1019@gmail.com>
2023-11-12 11:13:41 -0600

    dynamic routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HomePage } from "./HomePage";
 import { BlogPage } from "./BlogPage";
 import { ProfilePage } from "./ProfilePage";
 import { Menu } from "./Menu";
+import { BlogPost } from "./BlogPost";
 
 import "./App.css";
 
@@ -13,7 +14,8 @@ function App() {
         <Menu />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/blog/" element={<BlogPage />} />
+          <Route path="/blog/:slug" element={<BlogPost />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="*" element={<p>Not found</p>} />
         </Routes>
