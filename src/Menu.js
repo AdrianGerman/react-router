17fa3fbb20b0ec3882386095d1cef023069205ea
AdrianGerman <adrian.german1019@gmail.com>
2023-11-13 09:51:33 -0600

    public and private routes

--- src/Menu.js
+++ src/Menu.js
@@ -1,19 +1,27 @@
 import { NavLink } from "react-router-dom";
+import { useAuth } from "./auth";
 
 function Menu() {
+  const auth = useAuth();
   return (
     <nav>
       <ul>
-        {routes.map((route) => (
-          <li key={route.to}>
-            <NavLink
-              style={({ isActive }) => ({ color: isActive ? "red" : "blue" })}
-              to={route.to}
-            >
-              {route.text}
-            </NavLink>
-          </li>
-        ))}
+        {routes.map((route) => {
+          if (route.publicOnly && auth.user) return null;
+          if (route.private && !auth.user) return null;
+          return (
+            <li key={route.to}>
+              <NavLink
+                style={({ isActive }) => ({
+                  color: isActive ? "red" : "blue",
+                })}
+                to={route.to}
+              >
+                {route.text}
+              </NavLink>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
@@ -23,21 +31,27 @@ const routes = [];
 routes.push({
   to: "/",
   text: "Home",
+  private: false,
 });
 routes.push({
   to: "/blog",
   text: "Blog",
+  private: false,
 });
 routes.push({
   to: "/profile",
   text: "Profile",
+  private: true,
 });
 routes.push({
   to: "/login",
   text: "Login",
+  publicOnly: true,
+  private: false,
 });
 routes.push({
   to: "/Logout",
   text: "Logout",
+  private: true,
 });
 export { Menu };
