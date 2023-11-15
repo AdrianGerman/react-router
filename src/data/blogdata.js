3baa489cd314e3ef417d471ec2f62c772a95fafa
AdrianGerman <adrian.german1019@gmail.com>
2023-11-15 09:45:12 -0600

    create all data

--- src/data/blogdata.js
+++ src/data/blogdata.js
@@ -4,18 +4,26 @@ blogdata.push({
   slug: "que-es-react",
   content: "React es el mejor framework de JavaScript o Libreria?",
   author: "Adrian",
+  comments: [
+    {
+      content: "Hola muy buenas!\nPor cierto, me gusta tu melena",
+      author: "steve",
+    },
+  ],
 });
 blogdata.push({
   title: "¿Que es Vue?",
   slug: "que-es-vue",
   content: "Vue es el mejor framework de JavaScript o Libreria?",
   author: "AdrianGerman",
+  comments: [],
 });
 blogdata.push({
   title: "¿Que es Angular?",
   slug: "que-es-angular",
   content: "Angular es el mejor framework de JavaScript o Libreria?",
   author: "German",
+  comments: [],
 });
 
 export { blogdata };
---
+++ src/data/users.js
@@ -0,0 +1,40 @@
+import { ADMIN, USER } from "./roles";
+
+export const users = [
+  {
+    username: "german",
+    phonenumber: "333",
+    description: "I'm German 8)",
+    roles: [ADMIN],
+  },
+  {
+    username: "jack",
+    phonenumber: "333",
+    description: "I'm Jack (0,0)/",
+    roles: [ADMIN],
+  },
+  {
+    username: "gory",
+    phonenumber: "666",
+    description: "I'm gory <3",
+    roles: [ADMIN],
+  },
+  {
+    username: "steve",
+    phonenumber: "777",
+    description: "I'm steve 'cubito'",
+    roles: [ADMIN],
+  },
+  {
+    username: "adrian",
+    phonenumber: "333",
+    description: "I'm Adrian, caranabos",
+    roles: [USER],
+  },
+  {
+    username: "AdrianGerman",
+    phonenumber: "386162035",
+    description: "I'm Adrian German siseñor",
+    roles: [USER],
+  },
+];
